455fb47f7330aab79f5bc418555ad8381bb644e3
RyansS <monteiroryan2016@gmail.com>
2026-03-28 18:06:07 -0300

    Created model from computers, Started controller with a endpoint to Create a computer and defined route

---
+++ ComputersAPI/ComputersAPI/Controllers/ComputerController.cs
@@ -0,0 +1,20 @@
+﻿using ComputersAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputersAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ComputerController : ControllerBase
+    {
+        private static List<ComputerModel> AllComputers = new List<ComputerModel>();
+
+        [HttpPost]
+
+        public IActionResult CreateComputer([FromBody] ComputerModel computer)
+        {
+            computer.Id = AllComputers.Count + 1;
+        }
+
+    }
+}
---
+++ ComputersAPI/ComputersAPI/Models/ComputerModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputersAPI.Models
+{
+    public class ComputerModel
+    {
+        [Required(ErrorMessage = "Your model exceeded the maximum amount of characters")]
+        [MaxLength(255)]
+        public string Model { get; private set; }
+        //**********************
+        [Required(ErrorMessage = "The price is invalid")]
+        [Range(100, 150000)]
+        public decimal Price { get; private set; }
+        //**********************
+        [MaxLength(30)]
+        public string Os { get; private set; }
+        //**********************
+        [MaxLength (500)]
+        public string Description { get; private set; }
+        //**********************
+        public int Id { get; set; }
+
+    }
+}
