cd6671702eafa54ded8bfb731c5343861f29ee4a
RyansS <monteiroryan2016@gmail.com>
2026-04-01 14:45:19 -0300

    Clean project structure

---
+++ ToDoList/ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoListController : ControllerBase
+    {
+        private static List<ToDoListModel> Tasks = new List<ToDoListModel>();
+        private static int id = 0;
+
+        //Create Task
+        [HttpPost]
+
+        public CreatedAtActionResult PostCreateTask ([FromBody] ToDoListModel newTask)
+        {
+            newTask.Id = id++;
+            Tasks.Add(newTask);
+
+            return CreatedAtAction(nameof(GetTaskById), new {id = newTask.Id}, newTask);
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAllTasks ()
+        {
+            return Ok(Tasks);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetTaskById (int id)
+        {
+            var TaskFound = Tasks.FirstOrDefault(task => task.Id == id);
+            if (TaskFound == null) return NotFound();
+
+            return Ok(TaskFound);
+        }
+
+    }
+}
---
+++ ToDoList/ToDoList/Models/ToDoListModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDoListModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string TaskDescription { get; set; }
+
+        [MaxLength(25)]
+        public string TaskType { get; set; }
+
+        [Required]
+        public string TaskStatus { get; set; }
+
+
+    }
+}
